56923e7db993b9ac2b5b382687009e295af6f584
Matthew <myzerith1@gmail.com>
2023-08-03 16:26:12 -0400

    deleted code to tidy it up.
    makes pursing through easier

--- javascript.js
+++ javascript.js
@@ -1,44 +1,7 @@
-//This makes a random choice of rock, paper, or scissor.
 let choices = ["Rock", "Paper", "Scissor"];
-let computerReturn = null;
-//let randomChoice = choices[Math.floor(Math.random()*choices.length)];
-//let computerSelection = getComputerChoice();
-//let playerSelection = getPlayerChoice();
-//let computerSelection = getComputerSelection();
-//let playerSelection = getPlayerSelection();
 let trackComputerWins = 0;
 let trackPlayerWins = 0;
 let trackTies = 0;
-let tracker = ""
-
-//I need to find a way to get the player choice and computer choice
-//per round instead f declaring it at the beginning of the code
-/*
-function testing(){
-    let test = "1st variable";
-    console.log(test);
-}
-testing();
-*/
-
-/*
-function getComputerSelection(){
-//    return randomChoice
- 
-}
-function getPlayerSelection(){
-return prompt("What do you choose");
-}
-*/
-
-/*
-function getComputerChoice(){
-    return randomChoice;
-}
-function getPlayerChoice(){
-    return prompt("what do you choose");
-}
-*/
 
 function singleRound(){
     let computerSelection = choices[Math.floor(Math.random()*choices.length)];
@@ -76,7 +39,6 @@ function singleRound(){
     }
 }
 
-
 function game(){
     singleRound();
     console.log("Computer win tracks: " + trackComputerWins);
@@ -89,7 +51,3 @@ game();
 game();
 game();
 game();
-
-//getComputerSelection();
-//getPlayerSelection();
-//game();
